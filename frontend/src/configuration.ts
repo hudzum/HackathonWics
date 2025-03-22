c43f9869c319d73b4f65c9a7404b0a89a6c9b36e
hudzum <hudsonvu2@gmail.com>
2025-03-22 02:51:24 -0500

    LOL Removed console.log of API KEY

--- frontend/src/configuration.ts
+++ frontend/src/configuration.ts
@@ -9,8 +9,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(import.meta.env.VITE_apiKey);
-console.log("PJFOIWEJFOI")
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
