14d444d1a8f27a00f45e9dffcf5264b21ce79e8c
Muhammed Habibovic <72678073+muhammed-hab@users.noreply.github.com>
2025-03-23 13:37:23 -0500

    Update GAME_SERVER.ts

--- frontend/src/GAME_SERVER.ts
+++ frontend/src/GAME_SERVER.ts
@@ -1,2 +1,2 @@
-export const GAME_SERVER_CREATE = "http://74.249.97.213:80/create_game";
-export const GAME_SERVER_PLAY = "ws://74.249.97.213:80/game";
+export const GAME_SERVER_CREATE = "https://game.coincache.tech/create_game";
+export const GAME_SERVER_PLAY = "https://game.coincache.tech/game";
