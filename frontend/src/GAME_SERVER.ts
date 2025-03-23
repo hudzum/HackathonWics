4fcc5348dbf50197492fad57e953a576e3ac79e8
Muhammed Habibovic <72678073+muhammed-hab@users.noreply.github.com>
2025-03-23 13:10:15 -0500

    Update GAME_SERVER.ts

--- frontend/src/GAME_SERVER.ts
+++ frontend/src/GAME_SERVER.ts
@@ -1,2 +1,2 @@
-export const GAME_SERVER_CREATE = "http://167.96.127.87:3001/create_game";
-export const GAME_SERVER_PLAY = "ws://167.96.127.87:3001/game";
+export const GAME_SERVER_CREATE = "http://74.249.97.213:80/create_game";
+export const GAME_SERVER_PLAY = "ws://74.249.97.213:80/game";
